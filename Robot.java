c9bce80bd124e941d444524891c5f75d83e40546
Tara Williams <will4364@d.umn.edu>
2017-01-06 15:58:37 -0600

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team4009.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
@@ -41,7 +40,7 @@ public class Robot extends IterativeRobot {
 //        chooser.addObject("My Auto", new MyAutoCommand());
         SmartDashboard.putData("Auto mode", chooser);
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //::: Initialize ojbects! Each CANTalon # is the same channel number
+        //::: Initialize objects! Each CANTalon # is the same channel number
         //::: ex.  can0 is on channel, can1 is on channel 1
         /**
          * private CANTalon can0, can1,can2,can3,can4,can5,can6,can7;
@@ -60,10 +59,10 @@ public class Robot extends IterativeRobot {
         can6 = new CANTalon(6);
         can7 = new CANTalon(7);
         
-        ac = new RobotDrive(can0,can4);
-        eg = new RobotDrive(can1,can5);
-        db = new RobotDrive(can6,can2);
-        hf = new RobotDrive(can7,can3);
+        ac = new RobotDrive(can7,can4);
+        eg = new RobotDrive(can6,can2);
+        db = new RobotDrive(can1,can3);
+        hf = new RobotDrive(can5,can0);
 
     }
 	
@@ -78,13 +77,16 @@ public class Robot extends IterativeRobot {
 	
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
+		//System.out.println(" Twist " + joystick.getTwist());
+		//System.out.println(" X " + joystick.getX());
+		//System.out.println(" Y " + joystick.getY());
 	}
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
 	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
 	 * Dashboard, remove all of the chooser code and uncomment the getString code to get the auto name from the text box
-	 * below the Gyro
+	 * below the Gyroa
 	 *
 	 * You can add additional auto modes by adding additional commands to the chooser code above (like the commented example)
 	 * or additional comparisons to the switch structure below with additional strings & commands.
@@ -125,82 +127,226 @@ public class Robot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
+
+        //////////////////////////////////////////////////////////////////
+
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
-        //::: PSEUDOCODE ::://
-        /**
-         * FORWARD:  		if(joystick x = 0 and joystick y > 0) ac, db, hf to go forward, eg to stop
-         * BACKWARD: 		if(joystick x = 0 and joystick y < 0) ac, db, hf to go backward, eg to stop
-         * STRAFE LEFT: 	if(joystick x<0 and joystick y = 0) eg, ac(backward), bd to go forward, hf to stop
-         * STRAFE RIGHT:	if(joystick x>0 and joystick y = 0) eg(backwards), ac, bd(backward), hf to stop
-         * STRAFE UP LEFT:	if(joystick x<0 and joystick y>0) eg, db, hf, to go forward, ac to stop  
-         * STRAFE UP RIGHT 	if(joystick x>0 and joystick y>0) eg to go backward, ac, hf, to go forward, bd to stop
-         * STRAFE DOWN LEFT if(joystick x<0 and joystick y<0) eg to go forward, ac to go backward, hf to go backward, db to stop
-         * STRAFE DOWN RIGHT: if joystick x>0 and joystick y<0) eg to go backwards, hf to go backwards, db to go backwards, ac to stop
-         * 
-         */
+    
+    	double xPwr;
+    	double yPwr;
+    	double zPwr;
+    	double highPwr;
+    	double oddQuad;
+    	double evenQuad;
+    	double pScale;
+    	
+    	double DZ = 0.1;
+    	double twistDZ = 0.2;
+    	
+    	double xCur;
+    	double yCur;
+    	double zCur;
+    	double highCur;
+    	double oddCur;
+    	double evenCur;
+    	boolean isPressed;
+    	double pMag;
+    	
+    	while(isOperatorControl() && isEnabled()){
 
-        //:::::::::::::WRITE DEADZONE EVALUATION FUNCTION::::::::::::::::
-        double xMag;
-        double yMag;
-        double avgMag; 
-        int DZ = 10;
-        while(isOperatorControl() && isEnabled()){
-           
-            xMag = Math.abs(Math.pow(joystick.getX()/100, 3)); // Value cubed for exponential speed increase
-            yMag = Math.abs(Math.pow(joystick.getY()/100, 3)); // ^^
-            avgMag = (xMag + yMag)/2;
-        	//FORWARD
-        	if((joystick.getX() < DZ && joystick.getX() > -DZ) && (joystick.getY() > DZ)){
-        		ac.drive(yMag,0);
-        		eg.drive(0,0);
-        		db.drive(yMag,0);
-        		hf.drive(yMag,0);
-        		
-        	}//BACKWARD
-        	else if((joystick.getX() < DZ && joystick.getX() > -DZ) && joystick.getY() < -DZ){
-        		ac.drive(-yMag,0);
-        		eg.drive( 0,0);
-        		db.drive(-yMag,0);
-        		hf.drive(-yMag,0);
-        	}//LEFT
-        	else if(joystick.getX() < -DZ && (joystick.getY() < DZ && joystick.getY() > -DZ)){
-        		ac.drive(-xMag,0);
-        		eg.drive(xMag,0);
-        		db.drive(xMag,0);
-        		hf.drive(0,0);
-        	}//RIGHT
-        	else if(joystick.getX() > DZ && (joystick.getY() < DZ && joystick.getY() > -DZ)){
-        		ac.drive(xMag,0);
-        		eg.drive(-xMag,0);
-        		db.drive(-xMag,0);
-        		hf.drive(0,0);
-        	}//UP LEFT
-        	else if(joystick.getX() < -DZ && joystick.getY() > DZ){
-        		ac.drive(0,0);
-        		eg.drive(xMag,0);
-        		db.drive(avgMag,0);
-        		hf.drive(yMag,0);        		
-        	}//UP RIGHT
-        	else if(joystick.getX() > DZ && joystick.getY() > DZ){
-        		ac.drive(avgMag,0);
-        		eg.drive(-xMag,0);
-        		db.drive(0,0);
-        		hf.drive(yMag,0);
-        	}//DOWN LEFT
-        	else if(joystick.getX() < -DZ && joystick.getY() < -DZ){
-        		ac.drive(-avgMag,0);
-        		eg.drive(xMag,0);
-        		db.drive(0,0);
-        		hf.drive(-yMag,0);
-        	}//DOWN RIGHT
-        	else if(joystick.getX() > DZ && joystick.getY() < -DZ){
-        		ac.drive(0, 0);
-        		eg.drive(-xMag,0);
-        		db.drive(-avgMag,0);
-        		hf.drive(-yMag,0);
-        	}
-        }
+    		xPwr = joystick.getX();
+    		yPwr = -joystick.getY();
+    		zPwr = joystick.getTwist();
+    		highPwr = Math.max(Math.abs(xPwr), Math.abs(yPwr));
+    		oddQuad = yPwr - xPwr;
+    		evenQuad = yPwr + xPwr;	
+    		isPressed = joystick.getTrigger();
+    		pMag = (joystick.getThrottle() + 1) / 2;
+    		
+    	// Precision Mode
+    	   /**	the reason we add .5 to pMag then divide by 2 is to change the effective
+    		*	range of pMag from the throttle range of 0-1 to 0.25-0.75  
+    		*	A scale factor of zero would just be stopped, and a scale factor of 1 would 
+    		*	be full speed and thus pointless for precision mode.  You can change the
+    		*	function to create whatever precision range driver finds useful. 
+    		*/
+    		if (isPressed==true){
+    			pScale=((pMag+0.5)/2);
+    		}
+    		else{
+    			pScale=1;
+    		}
+    		
+    		 		
+    	// The Cur Variables 
+    	   /**	Cur variables are meant to smooth the drive speed coming in and out of
+    	 	*	the dead zone so that the robot drives less "choppy" at slower speeds
+    		*/
+    		
+    	// xCur
+    		if (Math.abs(xPwr)<DZ){
+    			xCur=0;
+    		}
+    		else{
+    			xCur= Math.signum(xPwr)*pScale*((Math.abs(xPwr)-DZ)*(1/(1-DZ)));
+    		}
+    		
+    		/**  
+    		 	//same code for xCur as above using "if" statements
+    			if (xPwr<0) {xCur= pScale*((xPwr+DZ)*(1/(1-DZ)));}
+    			if (xPwr>0) {xCur= pScale*((xPwr-DZ)*(1/(1-DZ)));}}
+    		*/
+    			
+    	
+    	// yCur
+    		if (Math.abs(yPwr)<DZ){
+    			yCur=0;
+    		}
+    		else{
+    			yCur= Math.signum(yPwr)*pScale*((Math.abs(yPwr)-DZ)*(1/(1-DZ)));
+    		}
+    			
+    		/**
+    			//same code for yCur as above using "if" statements
+    			if (yPwr<0) {yCur= pScale*((yPwr+DZ)*(1/(1-DZ)));}
+    			if (yPwr>0) {yCur= pScale*((yPwr-DZ)*(1/(1-DZ)));}}
+    		*/
+    	
+    	
+    	// zCur
+    		if (Math.abs(zPwr)<twistDZ){
+    			zCur=0;
+    		}
+    		else{
+    			zCur= Math.signum(zPwr)*pScale*((Math.abs(zPwr)-twistDZ)*(1/(1-twistDZ)));
+    		}
+    		
+    	// highCur
+    		if (Math.abs(highPwr)<DZ){
+    			highCur=0;
+    		}
+    		else{
+    			highCur= Math.signum(highPwr)*pScale*((Math.abs(highPwr)-DZ)*(1/(1-DZ)));
+    		}
+    		
+    	// oddCur
+    		if (Math.abs(oddQuad)<DZ){
+    			oddCur=0;
+    		}else{
+    			oddCur= Math.signum(oddQuad)*pScale*((Math.abs(oddQuad)-DZ)*(1/(1-DZ)));
+    		}
+    		
+    	// evenCur
+    		if (Math.abs(evenQuad)<DZ){
+    			evenCur=0;
+    		}
+    		else{
+    			evenCur= Math.signum(evenQuad)*pScale*((Math.abs(evenQuad)-DZ)*(1/(1-DZ)));
+    		}
+    						
+    				
+    		
+  //Code assumes motors c,g,b,f are reversed in direction
+    		
+			if(Math.abs(zPwr)>twistDZ){
+				ac.setLeftRightMotorOutputs(zCur, -zCur);
+				eg.setLeftRightMotorOutputs(zCur, -zCur);
+				db.setLeftRightMotorOutputs(zCur, -zCur);
+				hf.setLeftRightMotorOutputs(zCur, -zCur);
+			}
+			else{
+				
+			 // DeadZone is built into the Cur variables so separate DeadZone section not required
+				
+			 // X-Positive, Y-Positive
+				if(xPwr >= 0 && yPwr >= 0){
+				ac.drive(highCur,0);
+				eg.drive(xCur,0);
+				db.drive(oddCur,0);
+				hf.drive(yCur,0);
+				}
+				
+			// X-Negative, Y-Positive
+			if(xPwr < 0 && yPwr >= 0){
+				ac.drive(evenCur,0);
+				eg.drive(xCur,0);
+				db.drive(highCur,0);
+				hf.drive(yCur,0);
+				}
+			
+			// X-Negative, Y-Negative
+			if(xPwr < 0 && yPwr < 0){
+				ac.drive(-highCur,0);
+				eg.drive(xCur,0);
+				db.drive(oddCur,0);
+				hf.drive(yCur,0); 
+				}
+			
+			// X-Positive, Y-Negative
+			if(xPwr >= 0 && yPwr < 0){
+				ac.drive(evenCur,0);
+				eg.drive(xCur,0);
+				db.drive(-highCur,0);
+				hf.drive(yCur,0);	
+        		}
+			
+			}
+    		
+    			
+    /**	 Commented out old code in favor of Cur code
+
+    	//Code assumes motors c,g,b,f are reversed in direction
+    		
+    			if(zPwr <= -0.2 || zPwr >= 0.2){
+    				ac.setLeftRightMotorOutputs(zPwr, -zPwr);
+    				eg.setLeftRightMotorOutputs(zPwr, -zPwr);
+    				db.setLeftRightMotorOutputs(zPwr, -zPwr);
+    				hf.setLeftRightMotorOutputs(zPwr, -zPwr);
+				}
+    			else{
+					
+				  // DeadZone
+    				if(Math.abs(xPwr) < DZ && Math.abs(yPwr) < DZ){
+    				ac.drive(0,0);
+    				eg.drive(0,0);
+    				db.drive(0,0);
+    				hf.drive(0,0);
+    				
+    			}// X-Positive, Y-Positive
+    				if(xPwr >= 0 && yPwr >= 0){
+    				ac.drive(highPwr,0);
+    				eg.drive(xPwr,0);
+    				db.drive(oddQuad,0);
+    				hf.drive(yPwr,0);	
+
+    			}// X-Negative, Y-Positive
+    			if(xPwr < 0 && yPwr >= 0){
+    				ac.drive(evenQuad,0);
+    				eg.drive(xPwr,0);
+    				db.drive(highPwr,0);
+    				hf.drive(yPwr,0);
+
+    			}// X-Negative, Y-Negative
+    			if(xPwr < 0 && yPwr < 0){
+    				ac.drive(-highPwr,0);
+    				eg.drive(xPwr,0);
+    				db.drive(oddQuad,0);
+    				hf.drive(yPwr,0); 
+
+    			}// X-Positive, Y-Negative
+    			if(xPwr >= 0 && yPwr < 0){
+    				ac.drive(evenQuad,0);
+    				eg.drive(xPwr,0);
+    				db.drive(-highPwr,0);
+    				hf.drive(yPwr,0);	
+    			
+            		}
+    			}		
+    */
+			
+    			
+    	}
     }
     
     /**
@@ -212,3 +358,7 @@ public class Robot extends IterativeRobot {
     
    
 }
+
+
+
+
